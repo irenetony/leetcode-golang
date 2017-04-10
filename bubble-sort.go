6abe2d6dd6f7ad7afaf50b50930adc3f62d15a1f
berryjam <berryjamcoding@gmail.com>
2017-04-10 17:53:35 +0800

    bubble sort

---
+++ bubble-sort.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"leetcode/util"
+	"fmt"
+	"strings"
+)
+
+func BubbleSort(data []int) {
+	if data == nil {
+		return
+	}
+
+	for i := len(data) - 2; i >= 0; i-- {
+		hasSawped := false
+		for j := 0; j <= i; j++ {
+			if data[j] > data[j+1] {
+				data[j], data[j+1] = data[j+1], data[j]
+				hasSawped = true
+			}
+		}
+		if !hasSawped {
+			break
+		}
+	}
+}
+
+func main() {
+	//data := []int{3, 7, 8, 5, 4, 10, 15, 1, 2, 9}
+	//data := []int{}
+	//data := []int{3}
+	//data := []int{1, 2}
+	data := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+	BubbleSort(data)
+	tmpData := util.IntArray2StringArray(data)
+	fmt.Printf("Sorted data : %s", strings.Join(tmpData, ","))
+}
