1642b3e8343dc55908c102839ddeab698e9b0c34
berryjam <berryjamcoding@gmail.com>
2017-04-27 10:52:12 +0800

    pre-less-post-large

--- pre-less-post-large.go
+++ pre-less-post-large.go
@@ -14,39 +14,40 @@ func preLessPostLarge(array []int) []int {
 	res := make([]int, 0)
 	flags := make([]bool, len(array))
 
-	leftMax := array[0]
-	for i := 0; i < len(array); i++ {
-		if array[i] > leftMax {
-			leftMax = array[i]
-		} else {
-			if i == 0 {
-				continue
-			}
-			flags[i] = true
-		}
-	}
-	if array[0] > leftMax {
-		flags[0] = true
-	}
+	max := array[0]
+	min := array[0]
 
 	rightMin := array[len(array)-1]
 	for i := len(array) - 1; i >= 0; i-- {
 		if array[i] < rightMin {
 			rightMin = array[i]
+			min = array[i]
 		} else {
+			max = array[i]
 			if i == len(array)-1 {
 				continue
 			}
 			flags[i] = true
 		}
 	}
-	if array[len(array)-1] < rightMin {
+	if array[0] > min {
+		flags[0] = true
+	}
+	if array[len(array)-1] < max {
 		flags[len(array)-1] = true
 	}
 
-	for idx, excluded := range flags {
-		if !excluded {
-			res = append(res, array[idx])
+	leftMax := array[0]
+	for i := 0; i < len(array); i++ {
+		if array[i] > leftMax {
+			leftMax = array[i]
+		} else {
+			if i != 0 {
+				flags[i] = true
+			}
+		}
+		if !flags[i] {
+			res = append(res, array[i])
 		}
 	}
 
@@ -54,7 +55,7 @@ func preLessPostLarge(array []int) []int {
 }
 
 func main() {
-	array := []int{1, 6, 4, 5, 9, 10 }
+	array := []int{1, 6, 4, 5, 7, 9, 10 }
 	res := preLessPostLarge(array)
 	fmt.Printf("%s", strings.Join(util.IntArray2StringArray(res), ","))
 }
