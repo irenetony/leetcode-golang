27df2ce9c39a12e01f72377fe1fe945869dfad70
berryjam <berryjamcoding@gmail.com>
2017-05-13 14:01:51 +0800

    547. Friend Circles

---
+++ friend-circles.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"leetcode/util"
+	ds "leetcode/datastructure"
+)
+
+func findCircleNum(M [][]int) int {
+	res := 0
+	visitedNums := make([]bool, len(M))
+	for i := 0; i < len(M); i++ {
+		if !visitedNums[i] {
+			res++
+		}
+		queue := ds.NewQueue()
+		queue.Enqueue(i)
+		for !queue.IsEmpty() {
+			val, err := queue.Dequeue()
+			if err != nil {
+				panic(err)
+			}
+			num := val.(int)
+			visitedNums[num] = true
+			for j := 0; j < len(M); j++ {
+				if !visitedNums[j] && M[num][j] == 1 {
+					queue.Enqueue(j)
+				}
+			}
+		}
+	}
+
+	return res
+}
+
+func TestFindCircleNum(M [][]int) {
+	fmt.Println("Input:")
+	fmt.Println(util.TwoDimensionToString(M))
+	fmt.Printf("Output: %d", findCircleNum(M))
+	fmt.Println()
+}
+
+func main() {
+	array1 := [][]int{
+		{1, 1, 0},
+		{1, 1, 0},
+		{0, 0, 1},
+	}
+	TestFindCircleNum(array1)
+
+	array2 := [][]int{
+		{1, 1, 0},
+		{1, 1, 1},
+		{0, 1, 1},
+	}
+	TestFindCircleNum(array2)
+}
--- util/array-util.go
+++ util/array-util.go
@@ -1,6 +1,8 @@
 package util
 
-import "strconv"
+import (
+	"strconv"
+)
 
 func IntArray2StringArray(array []int) (res []string) {
 	for _, val := range array {
@@ -14,4 +16,46 @@ func RuneArray2StringArray(array []rune) (res []string) {
 		res = append(res, strconv.QuoteRune(val))
 	}
 	return
+}
+
+func TwoDimensionToString(array [][]int) string {
+	res := "["
+
+	for idx := range array {
+		if idx >= 1 {
+			res += " "
+		}
+		if idx != len(array)-1 {
+			if len(array) > 1 {
+				res += OneDimensionToString(array[idx]) + ",\n"
+			} else {
+				res += OneDimensionToString(array[idx])
+			}
+		} else {
+			res += OneDimensionToString(array[idx])
+		}
+	}
+
+	res += "]"
+	return res
+}
+
+func OneDimensionToString(array []int) string {
+	res := "["
+
+	for idx := range array {
+		if idx != len(array)-1 {
+			if len(array) > 1 {
+				res += strconv.Itoa(array[idx]) + ","
+			} else {
+				res += strconv.Itoa(array[idx])
+			}
+		} else {
+			res += strconv.Itoa(array[idx])
+		}
+	}
+
+	res += "]"
+
+	return res
 }
