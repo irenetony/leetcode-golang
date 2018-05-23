0fc34dd476b15152177651134c3af7ac991c2130
huangjinkun <huangjinkun1@huawei.com>
2018-05-23 15:29:48 +0800

    递归：通过二叉树先序和中序遍历构造二叉树

--- construct-binary-tree-from-preorder-and-inorder-traversal.go
+++ construct-binary-tree-from-preorder-and-inorder-traversal.go
@@ -6,85 +6,78 @@ import (
 )
 
 func buildTree(preorder []int, inorder []int) *ds.TreeNode {
-	if len(preorder) == 1 {
-		return &ds.TreeNode{Val: preorder[0]}
-	}
-
-	if len(inorder) == 1 {
-		return &ds.TreeNode{Val: inorder[0]}
-	}
-
-	if len(preorder) == 0 || len(inorder) == 0 {
+	if inorder == nil || len(inorder) == 0 || preorder == nil || len(preorder) == 0 {
 		return nil
 	}
 
-	rootVal := preorder[0]
-	rootNode := &ds.TreeNode{Val: rootVal}
-	leftPre, leftIn, rightPre, rightIn := getPart(rootVal, preorder, inorder)
+	if len(preorder) == 1 && len(inorder) == 1 {
+		return &ds.TreeNode{Val: preorder[0]}
+	}
 
-	leftNode := buildTree(leftPre, leftIn)
-	rightNode := buildTree(rightPre, rightIn)
+	root := &ds.TreeNode{Val: preorder[0]}
 
-	rootNode.Left = leftNode
-	rootNode.Right = rightNode
+	leftVals := getLeftVals(inorder, root.Val)
+	rightVals := getRightVals(inorder, root.Val)
+	left := buildTree(preorder[1:], leftVals)
+	right := buildTree(preorder[len(leftVals)+1:], rightVals)
 
-	return rootNode
+	root.Left = left
+	root.Right = right
+
+	return root
 }
 
-func getPart(root int, preorder, inorder []int) (leftPre, leftIn, rightPre, rightIn []int) {
-	var leftInIdx int
-	for i, v := range inorder {
-		if v != root {
-			leftInIdx = i
-		} else {
-			break
-		}
+func getLeftVals(inorder []int, root int) []int {
+	idx := 0
+	for inorder[idx] != root {
+		idx++
 	}
-	if (leftInIdx + 2) < len(inorder) {
-		rightIn = inorder[leftInIdx+2:]
-	}
-	if root != inorder[0] {
-		leftIn = inorder[:leftInIdx+1]
-	} else {
-		if root != inorder[leftInIdx+1] {
-			rightIn = inorder[leftInIdx+1:]
-		}
+	if idx == 0 {
+		return nil
 	}
 
-	var leftPreIdx int
-	for i, v := range preorder[1:] {
-		if inSlice(v, rightIn) {
-			leftPreIdx = i + 1
-			break
-		}
-	}
-	if leftPreIdx >= 1 {
-		leftPre = preorder[1:leftPreIdx]
-	}
-	rightPre = preorder[leftPreIdx:]
-
-	return
+	return inorder[:idx]
 }
 
-func inSlice(val int, slice []int) bool {
-	res := false
-
-	for _, v := range slice {
-		if val == v {
-			return true
-		}
+func getRightVals(inorder []int, root int) []int {
+	idx := 0
+	for inorder[idx] != root {
+		idx++
 	}
+	if idx == len(inorder)-1 {
+		return nil
+	}
+	return inorder[idx+1:]
+}
 
+func preorders(root *ds.TreeNode, res []int) []int {
+	if root == nil {
+		return res
+	}
+	res = append(res, root.Val)
+	res = preorders(root.Left, res)
+	res = preorders(root.Right, res)
+	return res
+}
+
+func inorders(root *ds.TreeNode, res []int) []int {
+	if root == nil {
+		return res
+	}
+	res = inorders(root.Left, res)
+	res = append(res, root.Val)
+	res = inorders(root.Right, res)
 	return res
 }
 
 func main() {
 	//preorder := []int{3, 9, 20, 15, 7}
 	//inorder := []int{9, 3, 15, 20, 7}
-	preorder := []int{1, 2}
-	inorder := []int{2, 1}
-	//preorder := []int{1, 5, 2, 3, 4, 6}
-	//inorder := []int{1, 2, 3, 4, 5, 6}
+	//preorder := []int{1, 2}
+	//inorder := []int{2, 1}
+	preorder := []int{1, 5, 2, 3, 4, 6}
+	inorder := []int{1, 2, 3, 4, 5, 6}
 	root := buildTree(preorder, inorder)
-	fmt.Printf("tree=%+v", root)
+	fmt.Printf("preorder=%+v\n", preorders(root, []int{}))
+	fmt.Printf("inorders=%+v\n", inorders(root, []int{}))
 }
