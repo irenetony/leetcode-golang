ffdfb59c305abeba58e689137ca9ecc75f484d43
huangjinkun <huangjinkun1@huawei.com>
2018-05-24 13:04:14 +0800

    添加、搜索单词

--- add-and-search-word.go
+++ add-and-search-word.go
@@ -2,138 +2,207 @@ package main
 
 import "fmt"
 
-var MY_DICITIONARY *MyDicitionary = &MyDicitionary{}
-
-type Dicitionary interface {
-	AddWord(word string)
-	Search(word string) bool
-}
-
-type MyDicitionary struct {
-	rootNodes map[rune]*Node
-}
-
 type Node struct {
-	ChildNodes map[rune]*Node
-	IsLeafNode bool
+	isWord bool
+	ch     rune
+	childs map[rune][]*Node
 }
 
-func (d *MyDicitionary) AddWord(word string) {
-	if d.rootNodes == nil {
-		d.rootNodes = make(map[rune]*Node)
-	}
-	curChildNodes := d.rootNodes
-	var parentNode *Node
+type WordDictionary struct {
+	root *Node
+}
 
-	for i, char := range word {
-		if curChildNodes != nil {
-			if _, ok := curChildNodes[char]; ok {
-				parentNode = curChildNodes[char]
-				curChildNodes = curChildNodes[char].ChildNodes
-			} else {
-				if i == len(word)-1 {
-					curChildNodes[char] = &Node{IsLeafNode: true}
-					curChildNodes = curChildNodes[char].ChildNodes
-				} else {
-					curChildNodes[char] = &Node{IsLeafNode: false}
-					parentNode = curChildNodes[char]
-					curChildNodes = curChildNodes[char].ChildNodes
+/** Initialize your data structure here. **/
+func Constructor() WordDictionary {
+	var dict WordDictionary
+	dict.root = new(Node)
+	dict.root.childs = make(map[rune][]*Node)
+	return dict
+}
+
+/** Adds a word into the data structure. **/
+func (this *WordDictionary) AddWord(word string) {
+	curNode := this.root
+	for i, r := range word {
+		if childs, ok := curNode.childs[r]; ok {
+			for _, child := range childs {
+				if child.ch == r {
+					curNode = child
+					break
 				}
 			}
 		} else {
-			curChildNodes = make(map[rune]*Node)
+			node := &Node{ch: r, childs: make(map[rune][]*Node)}
 			if i == len(word)-1 {
-				curChildNodes[char] = &Node{IsLeafNode: true}
-			} else {
-				curChildNodes[char] = &Node{IsLeafNode: false}
+				node.isWord = true
 			}
-			parentNode.ChildNodes = curChildNodes
-			parentNode = curChildNodes[char]
-			curChildNodes = curChildNodes[char].ChildNodes
+			curNode.childs[r] = append(curNode.childs[r], node)
+			curNode = node
 		}
 	}
 }
 
-func (d *MyDicitionary) Search(word string) bool {
-	curChildNodes := d.rootNodes
-	for i, char := range word {
-		if curChildNodes == nil {
-			return false
-		} else {
-			if char == '.' {
-				if i == len(word)-1 {
-					return true
-				} else {
-					for _, v := range curChildNodes {
-						if exists := SearchStarWord(word[i+1:], v.ChildNodes); exists {
+/** Returns if the word is in the data structure. A word could contain the dot character '.
+' to represent any one letter. **/
+func (this *WordDictionary) Search(word string) bool {
+	cur := this.root
+	for i, r := range word {
+		if r != '.' {
+			if childs, ok := cur.childs[r]; !ok {
+				return false
+			} else {
+				for _, child := range childs {
+					if child.ch == r {
+						if i == len(word)-1 && child.isWord {
 							return true
 						}
-					}
-					return false
-				}
-			} else {
-				if _, ok := curChildNodes[char]; !ok {
-					return false
-				} else {
-					if i == len(word)-1 {
-						return true
-					} else {
-						curChildNodes = curChildNodes[char].ChildNodes
+						cur = child
+						break
 					}
 				}
 			}
-		}
-	}
-
-	return false
-}
-
-func SearchStarWord(word string, childNodes map[rune]*Node) bool {
-	for i, char := range word {
-		if childNodes == nil {
-			return false
 		} else {
-			if char == '.' {
-				if i == len(word)-1 {
-					return true
-				} else {
-					for _, v := range childNodes {
-						if exists := SearchStarWord(word[i+1:], v.ChildNodes); exists {
-							return true
-						}
-					}
-					return false
-				}
-			} else {
-				if _, ok := childNodes[char]; !ok {
-					return false
-				} else {
-					if i == len(word)-1 {
+			if i == len(word)-1 {
+				return true
+			}
+			for _, nodes := range cur.childs {
+				for _, node := range nodes {
+					dict := Constructor()
+					dict.root = node
+					if dict.Search(word[i+1:]) {
 						return true
-					} else {
-						childNodes = childNodes[char].ChildNodes
 					}
 				}
 			}
+			return false
 		}
 	}
+
 	return false
 }
 
 func TestCase1() {
-	var i Dicitionary = MY_DICITIONARY
-	i.AddWord("bad")
-	i.AddWord("dad")
-	i.AddWord("mad")
-	fmt.Printf("word [%s] exist? %t\n", "pad", i.Search("pad"))
-	fmt.Printf("word [%s] exist? %t\n", "bad", i.Search("bad"))
-	fmt.Printf("word [%s] exist? %t\n", ".ad", i.Search(".ad"))
-	fmt.Printf("word [%s] exist? %t\n", "b..", i.Search("b.."))
-	fmt.Printf("word [%s] exist? %t\n", "mad", i.Search("mad"))
-	fmt.Printf("word [%s] exist? %t\n", "badd", i.Search("badd"))
-	fmt.Printf("word [%s] exist? %t\n", "...", i.Search("..."))
-	fmt.Printf("word [%s] exist? %t\n", "b.e", i.Search("b.e"))
-	fmt.Printf("word [%s] exist? %t\n", "ma.", i.Search("ma."))
+	obj := Constructor()
+	obj.AddWord("bad")
+	obj.AddWord("dad")
+	obj.AddWord("mad")
+	fmt.Printf("word [%s] exist? %t\n", "pad", obj.Search("pad"))
+	fmt.Printf("word [%s] exist? %t\n", "bad", obj.Search("bad"))
+	fmt.Printf("word [%s] exist? %t\n", ".ad", obj.Search(".ad"))
+	fmt.Printf("word [%s] exist? %t\n", "b..", obj.Search("b.."))
+	fmt.Printf("word [%s] exist? %t\n", "mad", obj.Search("mad"))
+	fmt.Printf("word [%s] exist? %t\n", "badd", obj.Search("badd"))
+	fmt.Printf("word [%s] exist? %t\n", "...", obj.Search("..."))
+	fmt.Printf("word [%s] exist? %t\n", "b.e", obj.Search("b.e"))
+	fmt.Printf("word [%s] exist? %t\n", "ma.", obj.Search("ma."))
+}
+
+func main() {
+	TestCase1()
+}
+package main
+
+import "fmt"
+type Node struct {
+	isWord bool
+	ch     rune
+	childs map[rune][]*Node
+}
+
+type WordDictionary struct {
+	root *Node
+}
+
+/** Initialize your data structure here. **/
+func Constructor() WordDictionary {
+	var dict WordDictionary
+	dict.root = new(Node)
+	dict.root.childs = make(map[rune][]*Node)
+	return dict
+}
+
+/** Adds a word into the data structure. **/
+func (this *WordDictionary) AddWord(word string) {
+	curNode := this.root
+	for i, r := range word {
+		if childs, ok := curNode.childs[r]; ok {
+			for _, child := range childs {
+				if child.ch == r {
+					curNode = child
+					break
+				}
+			}
+		} else {
+			node := &Node{ch: r, childs: make(map[rune][]*Node)}
+			if i == len(word)-1 {
+				node.isWord = true
+			}
+			curNode.childs[r] = append(curNode.childs[r], node)
+			curNode = node
+		}
+	}
+}
+
+/** Returns if the word is in the data structure. A word could contain the dot character '.
+' to represent any one letter. **/
+func (this *WordDictionary) Search(word string) bool {
+	cur := this.root
+	for i, r := range word {
+		if r != '.' {
+			if childs, ok := cur.childs[r]; !ok {
+				return false
+			} else {
+				for _, child := range childs {
+					if child.ch == r {
+						if i == len(word)-1 && child.isWord {
+							return true
+						}
+						cur = child
+						break
+					}
+				}
+			}
+		} else {
+			if i == len(word)-1 {
+				for _, nodes := range cur.childs {
+					for _, node := range nodes {
+						if node.isWord {
+							return true
+						}
+					}
+				}
+				return false
+			}
+			for _, nodes := range cur.childs {
+				for _, node := range nodes {
+					dict := Constructor()
+					dict.root = node
+					if dict.Search(word[i+1:]) {
+						return true
+					}
+				}
+			}
+			return false
+		}
+	}
+
+	return false
+}
+
+func TestCase1() {
+	obj := Constructor()
+	obj.AddWord("bad")
+	obj.AddWord("dad")
+	obj.AddWord("mad")
+	fmt.Printf("word [%s] exist? %t\n", "pad", obj.Search("pad"))
+	fmt.Printf("word [%s] exist? %t\n", "bad", obj.Search("bad"))
+	fmt.Printf("word [%s] exist? %t\n", ".ad", obj.Search(".ad"))
+	fmt.Printf("word [%s] exist? %t\n", "b..", obj.Search("b.."))
+	fmt.Printf("word [%s] exist? %t\n", "mad", obj.Search("mad"))
+	fmt.Printf("word [%s] exist? %t\n", "badd", obj.Search("badd"))
+	fmt.Printf("word [%s] exist? %t\n", "...", obj.Search("..."))
+	fmt.Printf("word [%s] exist? %t\n", "b.e", obj.Search("b.e"))
+	fmt.Printf("word [%s] exist? %t\n", "ma.", obj.Search("ma."))
 }
 
 func main() {
