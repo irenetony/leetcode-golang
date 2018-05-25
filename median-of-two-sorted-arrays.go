3e2de7f1bf52333d9f3aa73fb37e71b60b52f1e3
berryjam <berryjamcoding@gmail.com>
2018-05-25 22:30:09 +0800

    求两个有序数组的中位数，刚开始。本题可扩展为求两个有序数组的第k大数

---
+++ median-of-two-sorted-arrays.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+
+}
+
+func main() {
+	fmt.Printf("res=%f\n", findMedianSortedArrays([]int{1, 3}, []int{2}))
+	fmt.Printf("res=%f\n", findMedianSortedArrays([]int{1, 2}, []int{3, 4}))
+}
