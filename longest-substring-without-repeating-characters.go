6908098a3ac3f8f2e428f3849d36f7a507711210
Jinkun Huang <893131962@qq.com>
2018-05-31 10:04:14 +0800

    Update longest-substring-without-repeating-characters.go

--- longest-substring-without-repeating-characters.go
+++ longest-substring-without-repeating-characters.go
@@ -1,1 +1,29 @@
 
+func lengthOfLongestSubstring(s string) int {
+    res := 0
+    
+    runeMap := make(map[rune]bool)
+    
+    runeArr := []rune(s)
+    
+    for i:=0;i<len(runeArr);i++{
+        cur := 0
+        for j:=i;j<len(runeArr);j++ {
+            if _,ok := runeMap[runeArr[j]];!ok{
+                cur++
+                runeMap[runeArr[j]] = true
+                if cur> res {
+                    res =cur
+                }
+            }else{
+                if cur> res{
+                    res = cur
+                }
+                runeMap= make(map[rune]bool)
+                break
+            }
+        }
+    }
+    
+    return res
+}
